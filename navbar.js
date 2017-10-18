80bfa8a3c7295a1a0a7aefe311c3d94c60bac5b1
Habib Khan <hkhan@smbhd.com>
2017-10-18 13:23:02 -0500

    init

---
+++ navbar.js
@@ -0,0 +1,12 @@
+Vue.component('nav-bar',{
+    template: `
+        <nav class="ui stackable container four item menu">
+            <a class="item">Services</a>
+            <a class="item">Team</a>
+            <a class="item">Clients</a>
+            <a class="item">Contact Us</a>
+        </nav>`,
+    
+    
+    
+});
